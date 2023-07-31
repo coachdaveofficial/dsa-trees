74491be489bea8dfdd83548412a8accd0bfa4005
David Falson <david@falson.net>
2023-07-31 14:16:22 -0700

    finish tree and binary-tree

--- binary-tree.js
+++ binary-tree.js
@@ -17,28 +17,80 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if (!this.root) return 0;
 
+    function recursiveDepth(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return recursiveDepth(node.right) + 1;
+      if (node.right === null) return recursiveDepth(node.left) + 1;
+      return (
+        Math.min(recursiveDepth(node.left), recursiveDepth(node.right)) + 1
+      )
+    }
+    return recursiveDepth(this.root);
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if (!this.root) return 0;
 
+    function recursiveDepth(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return recursiveDepth(node.right) + 1;
+      if (node.right === null) return recursiveDepth(node.left) + 1;
+      return (
+        Math.max(recursiveDepth(node.left), recursiveDepth(node.right)) + 1
+      )
+    }
+    return recursiveDepth(this.root)
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let total = 0;
 
+    function recursiveSum(node) {
+      if (node === null) return 0;
+      let rightSum = recursiveSum(node.right);
+      let leftSum = recursiveSum(node.left);
+      // check to see if current total is higher than new possible sum
+      total = Math.max(total, node.val + leftSum + rightSum);
+      // compare which path (left or right) results in the larger sum
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+
+    }
+    recursiveSum(this.root)
+    return total;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if (!this.root) return null;
 
+    let queue = [this.root];
+    let closest = null;
+
+    while (queue.length) {
+      let currentNode = queue.shift();
+      let currentVal = currentNode.val;
+      let higherThanLowerBound = currentVal > lowerBound;
+      let shouldReassignClosest = currentVal < closest || closest === null;
+
+      if (higherThanLowerBound && shouldReassignClosest) {
+        closest = currentVal;
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+
+    return closest;
   }
 
   /** Further study!
@@ -68,7 +120,7 @@ class BinaryTree {
    * of two nodes in a binary tree. */
 
   lowestCommonAncestor(node1, node2) {
-    
+
   }
 }
 
--- tree.js
+++ tree.js
@@ -15,12 +15,46 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (this.root === null) return 0;
+
+    let total = this.root.val;
+
+    function recursiveSum(node) {
+      // iterate over children in node
+      for (let child of node.children) {
+        total += child.val;
+
+        // if child also has children, start process again
+        if (child.children.length > 0) {
+          recursiveSum(child)
+        }
+      }
+    }
+
+    recursiveSum(this.root);
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (this.root === null) return 0;
+
+    let evenArr = [];
+
+    function recursiveEvens(node) {
+      for (let child of node.children) {
+        if (child.val % 2 === 0) {
+          evenArr.push(child.val);
+        }
+        if (child.children.length > 0) {
+          recursiveEvens(child)
+        }
+      }
+    }
+
+    recursiveEvens(this.root);
+    return evenArr.length;
 
   }
 
@@ -28,7 +62,27 @@ class Tree {
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (this.root === null) return 0;
 
+    let count = 0;
+
+    if (this.root.val > lowerBound) {
+      count++
+    }
+
+    function recursiveGreater(node) {
+      for (let child of node.children) {
+        if (child.val > lowerBound) {
+          count++
+        }
+        if (child.children.length > 0) {
+          recursiveGreater(child)
+        }
+      }
+    }
+
+    recursiveGreater(this.root);
+    return count
   }
 }
 
